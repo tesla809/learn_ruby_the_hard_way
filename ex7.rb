c319bbe2d1c1d179912fe47f969cee1aa2df35e0
Anthony Albertorio <anthony.albertorio@gmail.com>
2014-10-14 13:17:15 -0400

    added example 7, ex7.rb

---
+++ ex7.rb
@@ -0,0 +1,27 @@
+puts "Mary had a little lamb."
+# if it was #{snow} there would be an undefined variable error.
+# however, the string #{'snow'} outputs word with no problem.
+# It is probably because of the single quotes around it.
+# but why use it like this?
+puts "Its fleece was white as #{'snow'}."
+puts "and everywhere that Mary went."
+#puts 10 dots on screen
+puts "." * 10 # what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r" 
+
+
+# watch that print vs. puts on this line; what's it do?
+print end1 + end2 + end3 + end4 + end5 + end6
+puts  end7 + end8 + end9 + end10 + end11 + end12
