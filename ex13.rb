b95c25e0f7eea3614c91cd08202509e3b5aa1d1b
Anthony Albertorio <anthony.albertorio@gmail.com>
2014-10-26 21:30:16 -0400

    added example 13, ex13.rb

---
+++ ex13.rb
@@ -0,0 +1,8 @@
+# Declared 3 variables and set them to ARGV 
+
+first, second, third, fourth = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+puts "Your fourth variable is: #{fourth}"
